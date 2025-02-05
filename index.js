ca233307be9fcec91d5a98c210add3b86c3ef042
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2025-02-05 18:37:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<head>
+
+  </head>
+
+<body>
+
+<h1>OpenAPI Documentation using Typescript and Express</h1>
+<hr/>
+Creating API documentation for an Express app using TypeScript can be done efficiently with tools like swagger-ui-express and swagger-jsdoc. This allows you to automatically generate API docs based on JSDoc-style comments or a Swagger definition.
+
+<b>Table Of Contents</a>
+<ul>
+<li>
+<a href="https://kaushiknatua12345.github.io/APIWithTSAndSwagger/swagger.json">Swagger UI</a>
+</li> 
+<li>
+<a href="https://github.com/kaushiknatua12345/APIWithTSAndSwagger/blob/master/README.md">Readme</a>
+</li> 
+</ul> 
+
+</body>
